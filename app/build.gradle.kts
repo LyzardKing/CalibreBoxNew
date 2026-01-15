07857e6bb42ee9ba758da2b2094900634fddcbc9
Galileo Sartor <galileo.sartor@gmail.com>
2026-01-15 13:07:39 +0000

    Bump version 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 2
-        versionName = "1.1.0"
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // This makes the key available in your Manifest
